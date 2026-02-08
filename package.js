b3e7a6fa939ba8d26c7ce362b3f87fae41f33a54
jigglekitten <jigglekitten@hotmail.com>
2026-02-08 14:58:20 -0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "clawdbot-cloud",
+  "version": "1.0.0",
+  "main": "server.js",
+  "type": "module",
+  "dependencies": {
+    "openclaw": "latest"
+  },
+  "scripts": {
+    "start": "node server.js"
+  }
+}
